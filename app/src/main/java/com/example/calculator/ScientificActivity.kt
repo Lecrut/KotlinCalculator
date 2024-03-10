0c18a193add0a4a4b58f1627c692df0a67d056dc
Lecrut <filip.lecrut12@gmail.com>
2024-03-10 16:25:02 +0100

    add "back button"

--- app/src/main/java/com/example/calculator/ScientificActivity.kt
+++ app/src/main/java/com/example/calculator/ScientificActivity.kt
@@ -1,11 +1,19 @@
 package com.example.calculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ScientificActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scientific)
+
+        val buttonBackClick = findViewById<Button>(R.id.backButton)
+        buttonBackClick.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/calculator/SimpleActivity.kt
+++ app/src/main/java/com/example/calculator/SimpleActivity.kt
@@ -1,11 +1,19 @@
 package com.example.calculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SimpleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simple)
+
+        val buttonBackClick = findViewById<Button>(R.id.backButton)
+        buttonBackClick.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
